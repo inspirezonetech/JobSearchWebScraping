c92227ad52790672b40b0d545d300939002a4083
AzizCode92 <azizbenothman76@gmail.com>
2020-12-19 17:01:08 +0100

    reformat config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 configuration file
 """
 cfg = {
-	"keyword": "Machine Learning",
-	"sender_password": "123pass",
-	"sender_email": "my_email@gmail.com",
-	"receiver_email": ["reciever@gmail.com", "reciever2@gmail.com"],
-	"subject_email": "This is my subject",
-	"body_email": "body"   
+    "keyword": "Machine Learning",
+    "sender_password": "123pass",
+    "sender_email": "my_email@gmail.com",
+    "receiver_email": ["reciever@gmail.com", "reciever2@gmail.com"],
+    "subject_email": "This is my subject",
+    "body_email": "body"
 }
