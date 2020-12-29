f7fba3621ffa4cd3218f802fdae20fa21d068d4e
Aziz <azizbenothman76@gmail.com>
2020-12-29 23:25:25 +0100

    feat: send jobs per e-mail (#30)
    * feat: send jobs per e-mail
    
    * delete main from mail.py
    
    * code reformat
    
    * update gitignore
    
    * reformat config.py
    
    * update sending email
    
    * update email body
    
    * fix sending file by email
    
    * reformat file
    
    * adapt review comments
    
    * blank the config options
    
    Co-authored-by: Fum <66768334+funbeedev@users.noreply.github.com>

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+configuration file
+"""
+cfg = {
+    "keyword": "",
+    "city": "",
+    "sender_password": "",
+    "sender_email": "",
+    "receiver_email": "",
+    "email_smtp": "",
+    "subject_email": "",
+    "body_email": ""
+}
--- job-search-web-scraping.py
+++ job-search-web-scraping.py
@@ -1,12 +1,58 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import smtplib
+import sys
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from os.path import basename
 from pathlib import Path
 
-import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from config import cfg
+
+
+def send_email(sender_email_address, email_password,
+               receiver_email_address, email_subject, email_smtp,
+               email_body, file_path):
+    """
+    sender_email_address: sender email address
+    email_password: sender email password
+    receiver_email_address: receiver email password
+    email_subject: email subject
+    email_smtp: email smtp server
+    email_body: email body
+    file_path: file to attach in the email
+    """
+    # create an email message object
+    message = MIMEMultipart()
+    # configure email headers
+    message['subject'] = email_subject
+    message['From'] = sender_email_address
+    message['To'] = receiver_email_address
+
+    # attach the text file
+    message.attach(MIMEText(email_body))
+    part = MIMEApplication(open(file_path).read())
+    part.add_header('Content-Disposition', 'attachment; filename="%s"' % basename(file_path))
+    message.attach(part)
+
+    # set smtp server and port
+    server = smtplib.SMTP(email_smtp, '587')
+    # identify this client to the SMTP server
+    server.ehlo()
+    # secure the SMTP connection
+    server.starttls()
+    server.ehlo()
+    # login to email account
+    server.login(sender_email_address, email_password)
+    # send email
+    server.send_message(message)
+    # close connection to server
+    server.quit()
 
 
 def indeed_job_search(*args):
-
     browser = None
 
     PATH_TO_GECKO_DRIVER = './geckodriver'
@@ -26,14 +72,14 @@ def indeed_job_search(*args):
         print("Unable to find a webdriver.")
         return
 
-    browser.get('https://www.indeed.com/worldwide')
+    browser.get('https://www.indeed.com')
 
     browser.implicitly_wait(5)
 
     search_bar = browser.find_element_by_name('q')
-    search_bar.send_keys('machine learning')
+    search_bar.send_keys(cfg['keyword'])
     search_bar = browser.find_element_by_name('l')
-    search_bar.send_keys('New York')
+    search_bar.send_keys(cfg['city'])
     search_bar.send_keys(Keys.ENTER)
 
     browser.implicitly_wait(5)
@@ -44,11 +90,16 @@ def indeed_job_search(*args):
     file.write("\n")
 
     for job_element in search_results:
-
         job_title = job_element.text
         job_link = job_element.get_attribute('href')
 
         file.write("%s | link: %s \n" % (job_title, job_link))
+    send_email(cfg["sender_email"], cfg["sender_password"],
+               cfg["receiver_email"],
+               cfg["subject_email"],
+               cfg["email_smtp"],
+               cfg["body_email"],
+               "job_search.txt")
 
     browser.close()
 
